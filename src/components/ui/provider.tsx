1eef781b61a3959be37efb0dfe630a3d490dde84
usman.naeem <usmaanbinnaeem@gmail.com>
2025-08-30 18:27:39 +0500

    Enhancement: Introduced a new theme configuration for Chakra UI with custom color scales and font settings

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,18 +1,8 @@
 "use client";
 
-import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
-
-const system = createSystem(defaultConfig, {
-  theme: {
-    tokens: {
-      fonts: {
-        heading: { value: "var(--font-rubik)" },
-        body: { value: "var(--font-rubik)" },
-      },
-    },
-  },
-});
+import { system } from "../theme";
 
 export function Provider(props: ColorModeProviderProps) {
   return (
